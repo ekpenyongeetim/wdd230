6ed3fa6458e9d9b4565b0af7b33b51b7c085280d
ekpenyongeetim <ekpenyongeetim@gmail.com>
2022-02-01 03:03:32 +0100

    git

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,10 +1,18 @@
 /* code for date in header*/
-const datefieldUK = document.querySelector("aside");
+const datefield = document.querySelector("date");
+const datefieldUK = document.querySelector("aside"); 
+
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
 const fulldateUK = new Intl.DateTimeFormat("en-UK", {
 	dateStyle: "full"
 }).format(now);
 
-datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
 
 /* code for hamburger menu*/
 function toggleMenu() {
