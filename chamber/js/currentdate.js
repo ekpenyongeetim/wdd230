75db23b6174024dde64d2324c0972296c09571f9
ekpenyongeetim <ekpenyongeetim@gmail.com>
2022-02-19 16:54:56 +0100

    commit

--- chamber/js/currentdate.js
+++ chamber/js/currentdate.js
@@ -1,3 +1,5 @@
-/* code for date in footer*/
+/* code for date in footer
+document.getElementById("lastmodified").innerHTML = document.lastModified
+*/
 document.getElementById("year").innerHTML = new Date().getFullYear();
-document.getElementById("modify").innerHTML = document.lastModified
+document.querySelector("#lastModified").textContent = `Last Modification: ${document.lastModified}`;
--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,17 +1,10 @@
 /* code for date in header*/
 const datefield = document.querySelector("#date");
-const datefieldUK = document.querySelector("aside"); 
-
 
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	now
 );
-const fulldateUK = new Intl.DateTimeFormat("en-UK", {
-	dateStyle: "full"
-}).format(now);
-
-
 datefield.innerHTML = `<em>${fulldate}</em>`;
 
 /* code for hamburger menu*/
@@ -23,17 +16,8 @@ function toggleMenu() {
  const x = document.getElementById("hamburgerBtn")
  x.onclick = toggleMenu;
 
- /* windchill code*/
-
- function CheckWind() {
-    let temp = parseInt(document.getElementById("temp").value);
-    let speed = parseInt(document.getElementById("wind").value);
-    let message = windChill (temp, speed);
-    document.getElementById("output").innerHTML = "Wind Chill is" + message; 
-}
-
-function windChill (temp, speed){
-    let f = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
-    return Math.round(f); 
-}
+ /* banner-close code*/
+ document.querySelector(".bannerclose").addEventListener("click", function() {
+    document.querySelector(".bannerclose").closest(".banner").style.display = "none";
+ });
 
