c8722da2bd33999ad0cfdc6ca5ac349539482040
ekpenyongeetim <ekpenyongeetim@gmail.com>
2022-02-21 00:37:40 +0100

    jyy

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,9 +1,9 @@
-let t = parseFloat(document.getElementById("#temp").textContent);
-let w = parseFloat(document.getElementById("#wind").textContent);
+let t = parseFloat(document.getElementById("temp").textContent);
+let s = parseFloat(document.getElementById("wind").textContent);
 let windchill = "";
 
 if (t <= 50 && s > 3) {
-  windchill = windchill(t,s);
+  windchill = windChill(t,s);
 } else {
   windchill = "N/A"
 }
@@ -11,6 +11,6 @@ if (t <= 50 && s > 3) {
 document.querySelector("#output").innerHTML = windchill;
 
 function windChill(temp, speed) {
-	let windChill =  message = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
-    return windChill;
+	let windChill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
+  return Math.round(windChill);
 }
